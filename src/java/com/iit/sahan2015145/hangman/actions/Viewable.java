b6710c2e8a037c554cb0e33d1cb5e3b8eca90331
Sahan Ranasinghe <sahan93knightz@gmail.com>
2015-11-15 01:47:15 +0530

    Word Processing Logic Partially Completed Hangman images added

--- src/java/com/iit/sahan2015145/hangman/actions/Viewable.java
+++ src/java/com/iit/sahan2015145/hangman/actions/Viewable.java
@@ -4,4 +4,10 @@ package com.iit.sahan2015145.hangman.actions;
  * Created by sahan on 11/14/15.
  */
 public interface Viewable {
+
+    String getGuessedText();
+
+    void showResult(String word, String message, String tries, String imgUrl);
+
+    void end(int result);
 }
