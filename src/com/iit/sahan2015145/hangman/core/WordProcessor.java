bf51e913f2712193baff925cbe0789b8d3ebb6af
sahan93knightz <sahan93knightz@gmail.com>
2015-11-14 22:37:51 +0530

    Basic Class Structure

--- src/com/iit/sahan2015145/hangman/core/WordProcessor.java
+++ src/com/iit/sahan2015145/hangman/core/WordProcessor.java
@@ -4,4 +4,8 @@ package com.iit.sahan2015145.hangman.core;
  * Created by sahan on 11/14/15.
  */
 public class WordProcessor {
+
+    public String getTextToDisplay(String word) {
+        return "X X X X";
+    }
 }
